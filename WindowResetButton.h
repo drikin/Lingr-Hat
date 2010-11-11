58ab994be79881d2a59905c85596cab390f54938
To3oo Mi2ukam1 <tmizukam@hkg.odn.ne.jp>
2010-11-12 00:18:02 +0900

    enable to reset window size

---
+++ WindowResetButton.h
@@ -0,0 +1,14 @@
+//
+//  WindowResetButton.h
+//  Lingr Hat
+//
+//  Created by Tomoo Mizukami on 10/11/12.
+//  Copyright 2010 tmiz.net. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WindowResetButton : NSButton {
+}
+
+@end
